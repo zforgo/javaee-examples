f5ba2ec7c33a532bb2fc1fb73d001b414fc7933c
zforgo <zforgo@gmail.com>
2016-11-04 14:26:36 +0100

    push up

---
+++ javaee-json-schema/src/main/java/hu/zforgo/jackson/app/LoadRestService.java
@@ -0,0 +1,7 @@
+package hu.zforgo.jackson.app;
+
+/**
+ * Created by spinner on 7/12/16.
+ */
+public class LoadRestService {
+}
---
+++ javaee-json-schema/src/test/java/hu/zforgo/examples/IDListSplitter.java
@@ -0,0 +1,7 @@
+package hu.zforgo.examples;
+
+/**
+ * Created by spinner on 7/12/16.
+ */
+public class IDListSplitter {
+}
